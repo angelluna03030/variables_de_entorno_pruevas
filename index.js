0eae2d798ab8a215d4e6e5cc011efc88e74435c7
angel luna <angelstivengarcialuna@gmail.com>
2024-04-28 00:39:21 -0500

    ultima prueva

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+//ANGEL L. estuvo aqui :D
+// 🐯 🐯 🐯
+//
 //process:es una variable gloval de
 //javascript.
 //                                     .env: nombre del archivo donde se 
@@ -8,6 +10,14 @@
 //                                                                        esten en .env
 //todo -->   process.                          env                        . HOTS
 //           ^^^^^^^                            ^^^ 
+require('dotenv').config();
 console.log(
     process.env.PORT
+)
+console.log(
+    process.env.INFO_SENCIBLE
+)
+
+console.log(
+    process.env.DB_PASSWORD
 )
