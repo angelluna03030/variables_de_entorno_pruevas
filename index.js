8646b3471e67b52323b5d647bed0498e33456054
angel luna <angelstivengarcialuna@gmail.com>
2024-04-28 00:19:12 -0500

    agregando comentarios

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+
+//process:es una variable gloval de
+//javascript.
+//                                     .env: nombre del archivo donde se 
+//                                     encuentran las variables.   
+//              
+//                                                                        el nombre de las variables que 
+//                                                                        esten en .env
+//todo -->   process.                          env                        . HOTS
+//           ^^^^^^^                            ^^^ 
 console.log(
-    process.env.LINUX
+    process.env.PORT
 )
-
-
-
